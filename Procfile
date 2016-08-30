d53dd2c8859e0e940194079c7c92dc6a8f6d0782
Zinur Mustafayev <zinur_mustafayev@epam.com>
2016-08-30 16:05:37 +0300

    change text

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -jar target/checker.jar
+web:  java -jar target/checker.jar
--- src/main/java/com/heroku/undertow/HelloWorldServer.java
+++ src/main/java/com/heroku/undertow/HelloWorldServer.java
@@ -8,7 +8,7 @@ public class HelloWorldServer {
     public static void main(final String[] args) {
         Undertow server = Undertow.builder().addHttpListener(8080, "localhost").setHandler(exchange -> {
             exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
-            exchange.getResponseSender().send("Hello World");
+            exchange.getResponseSender().send("Hello World and heroku");
         }).build();
         server.start();
     }
