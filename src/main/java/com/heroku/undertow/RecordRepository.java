bf25130dd72009cf415406ca8c8365ac9d78141e
Zinur Mustafayev <zinur_mustafayev@epam.com>
2016-08-30 19:19:11 +0300

    added repository

---
+++ src/main/java/com/heroku/undertow/RecordRepository.java
@@ -0,0 +1,8 @@
+package com.heroku.undertow;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecordRepository extends JpaRepository<Record, Long> {
+}
