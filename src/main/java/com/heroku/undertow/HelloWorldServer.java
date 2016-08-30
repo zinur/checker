168a506734ffcc1eb38a639c7fed6635974c6daf
Zinur Mustafayev <zinur_mustafayev@epam.com>
2016-08-30 15:30:01 +0300

    added initial files

---
+++ src/main/java/com/heroku/undertow/HelloWorldServer.java
@@ -0,0 +1,16 @@
+package com.heroku.undertow;
+
+import io.undertow.Undertow;
+import io.undertow.util.Headers;
+
+public class HelloWorldServer {
+
+    public static void main(final String[] args) {
+        Undertow server = Undertow.builder().addHttpListener(8080, "localhost").setHandler(exchange -> {
+            exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+            exchange.getResponseSender().send("Hello World");
+        }).build();
+        server.start();
+    }
+
+}
