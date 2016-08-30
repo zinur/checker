d21e963da05d79b303b97037aa5a3d97e3beffff
Zinur Mustafayev <zinur_mustafayev@epam.com>
2016-08-30 18:04:22 +0300

    addes system variable

--- src/main/java/com/heroku/undertow/HelloWorldServer.java
+++ src/main/java/com/heroku/undertow/HelloWorldServer.java
@@ -8,12 +8,15 @@ import io.undertow.util.Headers;
 public class HelloWorldServer {
 
     public static void main(final String[] args) {
+
         Optional<String> property = Optional.ofNullable(System.getProperty("server.port"));
 
-        Undertow server = Undertow.builder().addHttpListener(property.isPresent()? Integer.valueOf(property.get()):8080, "localhost").setHandler(exchange -> {
-            exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
-            exchange.getResponseSender().send("Hello World and heroku");
-        }).build();
+        Undertow server = Undertow.builder()
+                .addHttpListener(property.isPresent() ? Integer.valueOf(property.get()) : 8080, "localhost")
+                .setHandler(exchange -> {
+                    exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                    exchange.getResponseSender().send("Hello World and heroku");
+                }).build();
         server.start();
     }
 
